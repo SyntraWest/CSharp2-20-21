16a522e0c1d278fba0a0da9113275fde699a283e
Philip van Oosten <pvoosten@gmail.com>
2020-09-07 20:20:27 +0200

    Eerste Rosetta project

---
+++ Rosetta/Rosetta/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Rosetta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welke talen wil je gebruiken op je Rosetta-tabel?");
+
+            List<string> talen = new List<string>();
+            Console.WriteLine("Geef een taal in of STOP om te stoppen");
+            string taal = Console.ReadLine();
+            while (taal.ToLower().Trim() != "stop")
+            {
+                if (!string.IsNullOrWhiteSpace(taal))
+                    talen.Add(taal);
+                Console.WriteLine("Geef een taal in of STOP om te stoppen");
+                taal = Console.ReadLine();
+            }
+
+            // Geef hier de woorden per taal in
+
+            Console.WriteLine(string.Join('\t', talen));
+        }
+    }
+}
