004abe941a0ce46de6a53aa3b3352318b506cc76
Philip van Oosten <pvoosten@gmail.com>
2020-09-09 21:29:13 +0200

    Voorbeeld bestand openen met standaardprogramma

---
+++ Rosetta/VoorbeeldProcessEnFilesystem/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace VoorbeeldProcessEnFilesystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // lijst van alle procesnamen
+            //var processes = Process.GetProcesses();
+            //foreach (var proc in processes)
+            //{
+            //    Console.WriteLine(proc.ProcessName);
+            //}
+
+            var startinfo = new ProcessStartInfo
+            {
+                UseShellExecute = true,
+                FileName = @"C:\Users\pvoos\AppData\Local\Temp\hallo.txt",
+            };
+
+            Process.Start(startinfo);
+
+        }
+    }
+}
