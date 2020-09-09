220d082ee5afd2c0cba8647ae96d9511002594ef
Philip van Oosten <pvoosten@gmail.com>
2020-09-09 21:45:49 +0200

    Bestand openen, tekst lezen en schrijven van een bestand

--- Rosetta/VoorbeeldProcessEnFilesystem/Program.cs
+++ Rosetta/VoorbeeldProcessEnFilesystem/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
 
 namespace VoorbeeldProcessEnFilesystem
 {
@@ -14,13 +15,30 @@ namespace VoorbeeldProcessEnFilesystem
             //    Console.WriteLine(proc.ProcessName);
             //}
 
+            string pad = @"C:\Users\pvoos\AppData\Local\Temp\hallo.txt";
+
             var startinfo = new ProcessStartInfo
             {
                 UseShellExecute = true,
-                FileName = @"C:\Users\pvoos\AppData\Local\Temp\hallo.txt",
+                FileName = pad,
             };
 
             Process.Start(startinfo);
+
+            Console.WriteLine("Inhoud van het bestand:");
+            string inhoud = File.ReadAllText(pad);
+
+            Console.WriteLine(inhoud);
+
+            File.WriteAllText(pad, "Hallo\n");
+            File.AppendAllText(pad, "Ook hallo");
+            // inhoud van het bestand is nu gewijzigd
+
+
+            // Interessant voor volgende les (14/9):
+            // Volgende zijn beiden TextReaders:
+            // Console.In
+            // File.OpenText(pad)
 
         }
     }
