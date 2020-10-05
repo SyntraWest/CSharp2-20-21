a6077033dab8c49bc676bdb99ff09e3d1da861b8
Philip van Oosten <pvoosten@gmail.com>
2020-10-05 21:15:01 +0200

    RosettaTabel en RosettaTaal initiële implementatie

---
+++ Rosetta/Rosetta.Impl/RosettaTaal.cs
@@ -0,0 +1,54 @@
+﻿using Rosetta.Interface;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace Rosetta.Impl
+{
+    internal class RosettaTaal : ITaal
+    {
+
+        private readonly List<ITerm> termen;
+
+        public RosettaTaal(string taal, int index)
+        {
+            Index = index;
+            Taal = taal;
+            termen = new List<ITerm>();
+        }
+
+        public int Index { get; } // read-only property
+        // Een readonly property is een property met enkel
+        // een getter, zonder setter (ook zonder private setter)
+
+        public int AantalTermen => termen.Count;
+
+        public string Taal { get; }
+
+        public int MaximumTermLengte()
+        {
+            int maxLengte = 0;
+            foreach (var term in termen)
+            {
+                int len = term.Term.Length;
+                if(len > maxLengte)
+                {
+                    maxLengte = len;
+                }
+            }
+            return maxLengte;
+
+            // Voorbeeldje voor een beetje later:
+            // met Linq kan je dit veel korter noteren
+            // return termen.Max(t => t.Term.Length);
+        }
+
+        public int MinimumTermLengte()
+        {
+            int minLengte = int.MaxValue;
+            // rest is oefening.
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Rosetta/Rosetta.Impl/RosettaTabel.cs
@@ -0,0 +1,90 @@
+﻿using Rosetta.Interface;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Rosetta.Impl
+{
+    public class RosettaTabel : IRosettaTabel
+    {
+
+        private readonly List<ITaal> talen;
+        private readonly List<ITermCombinatie> termcombinaties;
+
+        public RosettaTabel()
+        {
+            talen = new List<ITaal>();
+            termcombinaties = new List<ITermCombinatie>();
+        }
+
+        public ITermCombinatie this[int index] => termcombinaties[index];
+
+        public IReadOnlyList<ITaal> Talen => talen.AsReadOnly();
+
+        public IEnumerator<ITermCombinatie> GetEnumerator()
+        {
+            return termcombinaties.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return termcombinaties.GetEnumerator();
+        }
+
+        public ITaal VoegTaalToe(string taal)
+        {
+            var taalObject = new RosettaTaal(taal, talen.Count);
+            talen.Add(taalObject);
+            return taalObject;
+        }
+
+
+        #region voorlopig niet geïmplementeerd
+        public IRosettaTabel MetEnkelDezeTermcombinaties(params int[] combinaties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IRosettaTabel MetWillekeurigeCombinaties(int aantal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SchudDoorElkaar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SorteerOpTaal(ITaal taal, bool oplopend)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SorteerOpTaal(ITaal taal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WisselTalen(int index1, int index2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WisselTermen(int index1, int index2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IRosettaTabel ZonderTaal(ITaal taal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IRosettaTabel ZonderTaal(int taal)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion voorlopig niet geïmplementeerd
+    }
+}
