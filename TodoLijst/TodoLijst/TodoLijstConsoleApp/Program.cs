605b3fd96221197e5fae21a57d8f9406247c6812
Philip van Oosten <pvoosten@gmail.com>
2020-10-19 19:14:00 +0200

    Een project voor Todo-lijsten

---
+++ TodoLijst/TodoLijst/TodoLijstConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TodoLijstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
---
+++ TodoLijst/TodoLijst/TodoLijstLibrary/TodoItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace TodoLijstLibrary
+{
+    public class TodoItem
+    {
+        public TodoItem()
+        {
+            Naam = "Nieuw TODO-item";
+            TegenWanneer = DateTime.MaxValue;
+            WanneerKlaar = null;
+        }
+
+        public string Naam { get; set; }
+
+        /// <summary>
+        /// Tegen wanneer moet dit klaar zijn
+        /// </summary>
+        public DateTime TegenWanneer { get; set; }
+
+        /// <summary>
+        /// Wanneer was dit klaar?
+        /// </summary>
+        public DateTime? WanneerKlaar { get; set; }
+
+        /// <summary>
+        /// Of dit item is afgewerkt.
+        /// </summary>
+        public bool IsKlaar => WanneerKlaar.HasValue;
+
+        public void WerkNuAf() => WanneerKlaar = DateTime.Now;
+    }
+}
